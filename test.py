9ccf16dea9c8a985994435348c1e0c5fc8eff10a
Enrique Gabriel <enriquegabrielcarmona@gmail.com>
2022-09-04 22:50:56 +0000

    Test2

--- test.py
+++ test.py
@@ -1,1 +1,25 @@
-Testing
+import requests
+import json
+from pprint import pprint
+
+
+router = {
+ "ip": "sandbox-iosxe-latest-1.cisco.com", 
+ "port" : "443", 
+ "Username" : "developer",
+ "Password" : "C1sco12345"
+
+}
+
+url = "https://sandbox-iosxe-latest-1.cisco.com/restconf/data/ietf-interfaces:interfaces"
+
+
+
+headers = {
+  'Content-Type': 'application/yang-data+json',
+  'Accept': 'application/yang-data+json'
+}
+
+response = requests.request("GET", url, headers=headers)
+
+print(response.text)
