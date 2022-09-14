4f488912e05a2d3ac3c502d5d9568dc99dca9f1c
Enrique Gabriel <enriquegabrielcarmona@gmail.com>
2022-09-14 02:42:16 +0000

    Get the Organization ID from Cisco Meraki

--- OrganizationId-Meraki.py
+++ OrganizationId-Meraki.py
@@ -1,4 +1,4 @@
-#Script to return the "Organization ID" from Cisco Meraki by Enrique Gabriel
+#Script to get the "Organization ID" from Cisco Meraki by Enrique Gabriel
 
 # 1. Import "requests" and "json" to make API calls and handle the data in JSON format.
 
@@ -11,21 +11,21 @@ url = "https://api.meraki.com/api/v1/organizations"
 
 # 3. Ask for the API Key. This is going to be the value for "X-Cisco-Meraki-API-Key" in the header.
 
-apiKey = input("Please enter the API Key: ")
+apiKey = input("Please enter the API Key:  ")
 
-headers = {
+header = {
   'X-Cisco-Meraki-API-Key': apiKey
 }
 
 # 4. Make the GET request
 
-getRequest = requests.get( url, headers=headers).json()
+getRequest = requests.get( url, headers=header).json()
 
 # 5. Ask for the Organization name. 
 
-orgName = input("Please enter the Organization name: ")
+orgName = input("Please enter the Organization name:  ")
 
-# 6. Go through every organization until you get the specific match based on the name you have provided. This will return the ID.
+# 6. Go through every organization until you get the specific match based on the name you have provided. This will return the exact ID.
 
 for organizations in getRequest:
     if organizations["name"] == orgName:
