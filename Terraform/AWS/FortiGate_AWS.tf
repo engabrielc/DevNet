9025579a9c4dd0d4a5e44c3b0ea5429cfb3f85c7
engabrielc <81327115+engabrielc@users.noreply.github.com>
2023-06-07 08:01:57 -0400

    Added port 443 to deploy FortiGate

--- Terraform/AWS/FortiGate_AWS.tf
+++ Terraform/AWS/FortiGate_AWS.tf
@@ -74,8 +74,8 @@ resource "aws_security_group" "allow_all" {
 
 ingress {
     description      = "Web"
-    from_port        = 80
-    to_port          = 80
+    from_port        = 443
+    to_port          = 443
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
     
